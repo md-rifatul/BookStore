a7ab04b81a1561468d149448e0fe7326bf0797cd
Md. Rifatul Islam <rifatuul@gmail.com>
2025-10-01 14:20:29 +0600

    create Book.cs model

---
+++ BookStore/Models/Book.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookStore.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        [Required,StringLength(100)]
+        public string Title { get; set; }
+        [Required, StringLength(50)]
+        public string Author { get; set; }
+        [Required, Range(0,1000)]
+        public decimal Price { get; set; }
+    }
+}

c615ca6ce30596b80c0828dc86f9b718a230ca00
Md. Rifatul Islam <rifatuul@gmail.com>
2025-10-02 16:26:21 +0600

    implement try and catch block

--- BookStore/Controllers/BookController.cs
+++ BookStore/Controllers/BookController.cs
@@ -1,6 +1,7 @@
 ﻿using BookStore.Models;
 using BookStore.Services.IServices;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace BookStore.Controllers
 {
@@ -13,13 +14,36 @@ namespace BookStore.Controllers
         }
         public IActionResult Index()
         {
-            var books = _bookService.GetAllBooks();
-            return View(books);
+            try
+            {
+                var books = _bookService.GetAllBooks();
+                return View(books);
+            }
+            catch(Exception e)
+            {
+                ViewBag.ErrorMessage = "An error occurred while loading books. Please try again later.";
+                return View(new List<Book>());
+            }
+
         }
         public IActionResult Details(int bookId)
         {
-            var book = _bookService.GetBook(bookId);
-            return View(book);
+            try
+            {
+                var book = _bookService.GetBook(bookId);
+                if(book == null)
+                {
+                    ViewBag.ErrorMessage = "The requested book was not found.";
+                    return View();
+                }
+                return View(book);
+            }
+            catch(Exception e)
+            {
+                ViewBag.ErrorMessage = "An error occurred while retrieving book details.";
+                return View();
+            }
+
         }
         public IActionResult Create()
         {
@@ -28,31 +52,109 @@ namespace BookStore.Controllers
         [HttpPost]
         public IActionResult Create(Book book)
         {
-            _bookService.AddBook(book);
-            return RedirectToAction("Index");
+            try
+            {
+                if(ModelState.IsValid)
+                {
+                    _bookService.AddBook(book);
+                    return RedirectToAction("Index");
+                }
+                return View(book);
+            }
+            catch(DbUpdateException e)
+            {
+                ViewBag.ErrorMessage = "Failed to add the book due to a database error.";
+                return View(book);
+            }
+            catch (Exception e)
+            {
+                ViewBag.ErrorMessage = "An unexpected error occurred while adding the book.";
+                return View(book);
+            }
+
         }
         public IActionResult Edit(int bookId)
         {
-            var book = _bookService.GetBook(bookId);
-            return View(book);
+            try
+            {
+                var book = _bookService.GetBook(bookId);
+                if (book == null)
+                {
+                    ViewBag.ErrorMessage = "The requested book was not found.";
+                    return View();
+                }
+                return View(book);
+            }
+            catch(Exception e)
+            {
+                ViewBag.ErrorMessage = "An error occurred while loading the book for editing.";
+                return View();
+            }
+
         }
         [HttpPost]
         public IActionResult Edit(Book book)
         {
-            _bookService.UpdateBook(book);
-            return RedirectToAction("Index");
+            try
+            {
+                if(ModelState.IsValid)
+                {
+                    _bookService.UpdateBook(book);
+                    return RedirectToAction("Index");
+                }
+                return View(book);
+            }
+            catch(DbUpdateException e)
+            {
+                ViewBag.ErrorMessage = "Failed to update the book due to a database error.";
+                return View(book);
+            }    
+            catch(Exception e)
+            {
+                ViewBag.ErrorMessage = "An unexpected error occurred while updating the book.";
+                return View(book);
+            }
+
         }
         public IActionResult Delete(int bookId)
         {
-             var book = _bookService.GetBook(bookId);
-            return View(book);
+            try
+            {
+                var book = _bookService.GetBook(bookId);
+                if(book==null)
+                {
+                    ViewBag.ErrorMessage = "The requested book was not found.";
+                    return View();
+                }
+                return View(book);
+            }
+            catch(Exception e)
+            {
+                ViewBag.ErrorMessage = "An error occurred while loading the book for deletion.";
+                return View();
+            }
+
         }
 
         [HttpPost,ActionName("Delete")]
         public IActionResult DeleteConfirmed(Book book)
         {
-            _bookService.DeleteBook(book);
-            return RedirectToAction("Index");
+            try
+            {
+                _bookService.DeleteBook(book);
+                return RedirectToAction("Index");
+            }
+            catch (DbUpdateException e)
+            {
+                ViewBag.ErrorMessage = "Failed to delete the book due to a database error.";
+                return View(book);
+            }
+            catch (Exception e)
+            {
+                ViewBag.ErrorMessage = "An unexpected error occurred while deleting the book.";
+                return View(book);
+            }
+
         }
     }
 }
